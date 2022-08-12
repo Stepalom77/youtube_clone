71531e391909569aafb2e68995cc628403b02793
Stepalom77 <stepalom22@gmail.com>
2022-08-12 12:14:08 -0500

    users middlewares added

--- server/routes/usersRoutes.ts
+++ server/routes/usersRoutes.ts
@@ -1,16 +1,16 @@
 import { Express } from "express";
 import { UsersController } from "../controllers/usersController";
-
+import {auth, requiresAuth} from 'express-openid-connect'
 export class UsersRoutes {
     public userController: UsersController = new UsersController();
     public routes(app: Express): void {
         app.route('/api/users')
-        .get(this.userController.getUser)
+        .get(this.userController.getUsers, requiresAuth())
         .post(this.userController.createUser)
 
         app.route('/api/users/:id')
-        .get(this.userController.getUsers)
-        .put(this.userController.updateUser)
-        .delete(this.userController.deleteUser)
+        .get(this.userController.getUser, requiresAuth())
+        .put(this.userController.updateUser, requiresAuth())
+        .delete(this.userController.deleteUser, requiresAuth())
     } 
 }
