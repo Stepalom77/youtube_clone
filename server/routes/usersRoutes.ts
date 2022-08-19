b89ac010f7841e00d9c487bdeb2979de6764400b
Stepalom77 <stepalom22@gmail.com>
2022-08-18 21:56:51 -0500

    adding permission middleware

--- server/routes/usersRoutes.ts
+++ server/routes/usersRoutes.ts
@@ -1,16 +1,16 @@
 import { Express } from "express";
 import { UsersController } from "../controllers/usersController";
-//import {requiresAuth} from 'express-openid-connect'
+import { requiresAuth } from "express-openid-connect";
 import { checkJwt } from "../middlewares/authMiddleware";
 export class UsersRoutes {
     public userController: UsersController = new UsersController();
     public routes(app: Express): void {
         app.route('/api/users')
         .get(checkJwt, this.userController.getUsers)
-        .post(this.userController.createUser)
+        .post(checkJwt,this.userController.createUser)
 
         app.route('/api/users/:id')
-        .get(checkJwt, this.userController.getUser)
+        .get(checkJwt, requiresAuth(), this.userController.getUser)
         .put(checkJwt, this.userController.updateUser)
         .delete(checkJwt, this.userController.deleteUser)
     } 
